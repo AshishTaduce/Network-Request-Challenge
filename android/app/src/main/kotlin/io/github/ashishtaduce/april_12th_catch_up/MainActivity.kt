67b1529f7240948056aaab04d4ab4d161d420a5f
AshishTaduce <ashishteddy2@gmail.com>
2020-04-12 21:15:39 +0530

    Commit #1

---
+++ android/app/src/main/kotlin/io/github/ashishtaduce/april_12th_catch_up/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.github.ashishtaduce.april_12th_catch_up
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
